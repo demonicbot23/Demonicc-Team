04f0945176b0215ef84359bb52c3a262e9e1c792
demonicbot23 <30479145+demonicbot23@users.noreply.github.com>
2017-07-28 00:17:58 +0300

    Update rank.lua

--- plugins/rank.lua
+++ plugins/rank.lua
@@ -3,29 +3,29 @@
 local function run(msg, matches)
 local group = load_data('bot/group.json')
 local addgroup = group[tostring(msg.chat_id)]
-local setowner = redis:get('setowner'..msg.chat_id_)
-local promote = redis:get('promote'..msg.chat_id_)
-local demote = redis:get('demote'..msg.chat_id_)
+local setowner = redis:get('رفع مدير'..msg.chat_id_)
+local promote = redis:get('رفع ادمن'..msg.chat_id_)
+local demote = redis:get('تنزيل ادمن'..msg.chat_id_)
 if addgroup then
     if matches[1] == 'owner' then
-	 pm = group[tostring(msg.chat_id_)]['set_owner']
+	 pm = group[tostring(msg.chat_id_)]['رفع مدير']
 	 tg.sendMessage(msg.chat_id_, 0, 1,'owner['..pm..']', 1, 'html')
 	end
     if setowner then
-         group[tostring(msg.chat_id_)]['set_owner'] = tostring(msg.from_id)
+         group[tostring(msg.chat_id_)]['رفع مدير'] = tostring(msg.from_id)
          save_data(_config.group.data, group)
          tg.sendMessage(msg.chat_id, 0, 0,  'تم رفعك من قبل ['..setowner..'] المطور ['..msg.from_id..'] لمدير البوت .', 0)
 		 redis:del('setowner'..msg.chat_id_ ,true)
     end
 if promote then
-         group[tostring(msg.chat_id_)]['moderators'][tostring(msg.from_id)]  = tostring(msg.from_id)
+         group[tostring(msg.chat_id_)]['رفع ادمن'][tostring(msg.from_id)]  = tostring(msg.from_id)
          save_data(_config.group.data, group)
 pm = 'تم رفعك '..msg.from_id..' ادمن في البوت'
          tg.sendMessage(msg.chat_id_, 0, 1,pm, 1, 'html')
 		 redis:del('promote'..msg.chat_id_ ,true)
     end
 if demote then
-         group[tostring(msg.chat_id_)]['moderators'][tostring(msg.from_id)] = nil
+         group[tostring(msg.chat_id_)]['تنزيل ادمن'][tostring(msg.from_id)] = nil
          save_data(_config.group.data, group)
 pm = 'تم حذفك '..msg.from_id..' من ادمنية البوت'
          tg.sendMessage(msg.chat_id_, 0, 1,pm, 1, 'html')
@@ -70,13 +70,13 @@ end
 end
 return {
   patterns = {
-    "^[#!/](setowner)$",
- "^[#!/](owner)$",
-	"^[#!/](setowner) (.+)$",
-    "^[#!/](promote)$",
-	"^[#!/](promote) (.+)$",
-    "^[#!/](demote)$",
-	"^[#!/](demote) (.+)$",
+    "^[#!/](رفع مدير)$",
+ "^[#!/](المدير)$",
+	"^[#!/](رفع مدير) (.+)$",
+    "^[#!/](رفع ادمن)$",
+	"^[#!/](رفع ادمن) (.+)$",
+    "^[#!/](تنزيل ادمن)$",
+	"^[#!/](تنزيل ادمن) (.+)$",
 	"^(ownerset)$",
   },
   run = run
